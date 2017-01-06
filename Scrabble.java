4f7964a2be2553c145fa4739257e5dbc5bb7380a
srahman7 <sabiharahman725@gmail.com>
2017-01-05 23:32:21 -0500

    scoring is done

--- Scrabble.java
+++ Scrabble.java
@@ -1,5 +1,8 @@
 import java.util.*;
 import java.io.*;
+import javax.swing.*;
+import java.awt.*;//NEW STUFF!
+import java.awt.event.*;
 
 public class Scrabble {
     private String[][]grid;
@@ -7,6 +10,11 @@ public class Scrabble {
     private int wordCount;
     private boolean start;
     private ArrayList<String> Words;
+    private Container pane;
+    private JButton submit;
+    private String[] letterRack;
+    private int score;
+    
    
     
     public Scrabble(){
@@ -16,6 +24,12 @@ public class Scrabble {
 	wordCount = 0;
 	start = true;
 	Words = new ArrayList<String>();
+	score=0;
+	//submit.addActionListener(this);
+	//submit.setActionCommand(subWord();
+	
+	
+	
     }
 
     public void clear(){
@@ -92,6 +106,39 @@ public class Scrabble {
 	
     }
 
+    public int addScore(String word){
+	for (int x =0; x+1 <= word.length();x++){
+	    if ( (word.substring(x,x+1)).equals("A") || (word.substring(x,x+1)).equals("E") || (word.substring(x,x+1)).equals("I") || (word.substring(x,x+1)).equals("O") || (word.substring(x,x+1)).equals("U") || (word.substring(x,x+1)).equals("L") || (word.substring(x,x+1)).equals("N") || (word.substring(x,x+1)).equals("R") || (word.substring(x,x+1)).equals("S") || (word.substring(x,x+1)).equals("T") ){
+		score++;
+	    }
+	    if ( (word.substring(x,x+1)).equals("D") || (word.substring(x,x+1)).equals("G") ){
+		score+=2;
+	    }
+	    if ( (word.substring(x,x+1)).equals("B") || (word.substring(x,x+1)).equals("C") || (word.substring(x,x+1)).equals("M") || (word.substring(x,x+1)).equals("P")  ){
+		score+=3;
+	    }
+	    if ( (word.substring(x,x+1)).equals("F") || (word.substring(x,x+1)).equals("H") || (word.substring(x,x+1)).equals("W") || (word.substring(x,x+1)).equals("Y") || (word.substring(x,x+1)).equals("V") ){
+		score+=4;
+	    }
+	    if ( (word.substring(x,x+1)).equals("K") ){
+		score+=5;
+	    }
+	    if ( (word.substring(x,x+1)).equals("J") || (word.substring(x,x+1)).equals("X") ){
+		score+=8;
+	    }
+	    if ( (word.substring(x,x+1)).equals("Q") || (word.substring(x,x+1)).equals("Z") ){
+		score+=10;
+	    }
+	    else {
+		score+=0;
+	    }
+	}
+	return score;
+	
+		
+	
+    }
+
     public String toString() {
 	String str = "[";
 	
@@ -129,6 +176,7 @@ public class Scrabble {
 	System.out.println(a.toString());
 	a.horWords();
 	a.verWords();
+	System.out.println(a.addScore("DOG"));
         System.out.println(a.getWords());
 	System.out.println(a.checkAllWords());
 	//	System.out.println(checkWord("sghheu"));
