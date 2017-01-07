0ec170990f0b3a961d6823a9217c13c83d7f61a5
srahman7 <sabiharahman725@gmail.com>
2017-01-06 20:59:29 -0500

    finished words added

--- Scrabble.java
+++ Scrabble.java
@@ -9,8 +9,8 @@ public class Scrabble {
     private int letterCount;
     private int wordCount;
     private boolean start;
-    private ArrayList<String> Words;
-    private ArrayList<String> WordsAdded;
+    private  ArrayList<String> Words;
+    private  ArrayList<String> WordsAdded;
     private Container pane;
     private JButton submit;
     private String[] letterList;
@@ -26,7 +26,9 @@ public class Scrabble {
 	wordCount = 0;
 	start = true;
 	Words = new ArrayList<String>();
-	score=0;
+	WordsAdded= new ArrayList<String>();
+	score1=0;
+	score2=0;
 	//submit.addActionListener(this);
 	//submit.setActionCommand(subWord();
 	
@@ -41,6 +43,14 @@ public class Scrabble {
 	    }
 	}
     }
+
+    public void clearWords(){
+	Words.clear();
+    }
+
+    public void clearWordsAdded(){
+	WordsAdded.clear();
+    }
     
     public void addLetter(String letter, int row, int col){
 	grid[row][col] = letter;
@@ -48,6 +58,7 @@ public class Scrabble {
 
     public void horWords(){
 	String hor = "";
+	int id=0;
 	
 	for(int row = 0; row < 15; row++){
 	    for(int col = 0; col < 15; col++){
@@ -57,7 +68,8 @@ public class Scrabble {
 	
 	for(String word: hor.split("\\.")){
 	    if(word.length() > 1){
-	    Words.add(word);
+	    Words.add(word+id);
+	    id++;
 	    }
 	}
     }
@@ -65,7 +77,7 @@ public class Scrabble {
  
     public void verWords(){
 	String ver = "";
-	
+	int id=0;
 	for(int col = 0; col < 15; col++){
 	    for(int row = 0; row < 15; row++){
 		ver += grid[row][col];
@@ -74,11 +86,14 @@ public class Scrabble {
 	
 	for(String word: ver.split("\\.")){
 	    if(word.length() > 1){
-		Words.add(word);
+		Words.add(word+id);
+		id++;
 	    }
 	}
     }
     public void horWordsAdded(){
+	
+	int id=0;
 	String hor = "";
 	
 	for(int row = 0; row < 15; row++){
@@ -89,7 +104,8 @@ public class Scrabble {
 	
 	for(String word: hor.split("\\.")){
 	    if(word.length() > 1){
-	    WordsAdded.add(word);
+	    WordsAdded.add(word+id);
+	    id++;
 	    }
 	}
     }
@@ -114,16 +130,32 @@ public class Scrabble {
     }
 
     public String wordAdded(){
+	boolean check = false;
 	for (int x = 0; x < WordsAdded.size(); x++){
+	    int lastCheck = 0;
 	    for (int i=0; i <Words.size(); i++){
-		if (WordsAdded.get(x).equals(Words.get(i))
-					     }
-		    }
-	
+		if( !(WordsAdded.get(x)).equals(Words.get(i)) ){
+		    check = false;
+		    lastCheck++;
+		}
+		else{
+		    check=true;
+		    lastCheck=0;
+		}
 	    }
+	    if (check == false && lastCheck==Words.size()){
+		return WordsAdded.get(x);
+	    }
+	}
+	return "Word not added";
+    }
 
-	    public boolean checkAllWords(){
-		for(int i = 0; i < Words.size(); i++){
+    public String wordAddedXtra(){
+	return WordsAdded.get(WordsAdded.size()-1);
+    }
+
+    public boolean checkAllWords(){
+	for(int i = 0; i < Words.size(); i++){
 	    if(checkWord(Words.get(i)) == false){
 		return false;
 	    }
@@ -150,34 +182,66 @@ public class Scrabble {
 	
     }
 
-    public static int addScore(String word, int score){
+    public int addScore1(String word){
 	for (int x =0; x+1 <= word.length();x++){
 	    if ( (word.substring(x,x+1)).equals("A") || (word.substring(x,x+1)).equals("E") || (word.substring(x,x+1)).equals("I") || (word.substring(x,x+1)).equals("O") || (word.substring(x,x+1)).equals("U") || (word.substring(x,x+1)).equals("L") || (word.substring(x,x+1)).equals("N") || (word.substring(x,x+1)).equals("R") || (word.substring(x,x+1)).equals("S") || (word.substring(x,x+1)).equals("T") ){
-		score++;
+		score1++;
 	    }
 	    if ( (word.substring(x,x+1)).equals("D") || (word.substring(x,x+1)).equals("G") ){
-		score+=2;
+		score1+=2;
 	    }
 	    if ( (word.substring(x,x+1)).equals("B") || (word.substring(x,x+1)).equals("C") || (word.substring(x,x+1)).equals("M") || (word.substring(x,x+1)).equals("P")  ){
-		score+=3;
+		score1+=3;
 	    }
 	    if ( (word.substring(x,x+1)).equals("F") || (word.substring(x,x+1)).equals("H") || (word.substring(x,x+1)).equals("W") || (word.substring(x,x+1)).equals("Y") || (word.substring(x,x+1)).equals("V") ){
-		score+=4;
+		score1+=4;
 	    }
 	    if ( (word.substring(x,x+1)).equals("K") ){
-		score+=5;
+		score1+=5;
 	    }
 	    if ( (word.substring(x,x+1)).equals("J") || (word.substring(x,x+1)).equals("X") ){
-		score+=8;
+		score1+=8;
 	    }
 	    if ( (word.substring(x,x+1)).equals("Q") || (word.substring(x,x+1)).equals("Z") ){
-		score+=10;
+		score1+=10;
 	    }
 	    else {
-		score+=0;
+		score1+=0;
 	    }
 	}
-	return score;
+	return score1;
+	
+		
+	
+    }
+        public int addScore2(String word){
+	for (int x =0; x+1 <= word.length();x++){
+	    if ( (word.substring(x,x+1)).equals("A") || (word.substring(x,x+1)).equals("E") || (word.substring(x,x+1)).equals("I") || (word.substring(x,x+1)).equals("O") || (word.substring(x,x+1)).equals("U") || (word.substring(x,x+1)).equals("L") || (word.substring(x,x+1)).equals("N") || (word.substring(x,x+1)).equals("R") || (word.substring(x,x+1)).equals("S") || (word.substring(x,x+1)).equals("T") ){
+		score2++;
+	    }
+	    if ( (word.substring(x,x+1)).equals("D") || (word.substring(x,x+1)).equals("G") ){
+		score2+=2;
+	    }
+	    if ( (word.substring(x,x+1)).equals("B") || (word.substring(x,x+1)).equals("C") || (word.substring(x,x+1)).equals("M") || (word.substring(x,x+1)).equals("P")  ){
+		score2+=3;
+	    }
+	    if ( (word.substring(x,x+1)).equals("F") || (word.substring(x,x+1)).equals("H") || (word.substring(x,x+1)).equals("W") || (word.substring(x,x+1)).equals("Y") || (word.substring(x,x+1)).equals("V") ){
+		score2+=4;
+	    }
+	    if ( (word.substring(x,x+1)).equals("K") ){
+		score2+=5;
+	    }
+	    if ( (word.substring(x,x+1)).equals("J") || (word.substring(x,x+1)).equals("X") ){
+		score2+=8;
+	    }
+	    if ( (word.substring(x,x+1)).equals("Q") || (word.substring(x,x+1)).equals("Z") ){
+		score2+=10;
+	    }
+	    else {
+		score2+=0;
+	    }
+	}
+	return score2;
 	
 		
 	
@@ -208,21 +272,55 @@ public class Scrabble {
 	return (Words.toString());
     }
 
+   
+    public String getWordsAdded(){
+	return (WordsAdded.toString());
+    } 
+
     public static void main(String[] args){
 	
 	Scrabble a = new Scrabble();
 	a.addLetter("H", 10, 10);
 	a.addLetter("E", 10, 11);
-	a.addLetter("D", 11, 8);
-	a.addLetter("O", 11, 9);
-	a.addLetter("O", 12, 8);
-	a.addLetter("I", 13, 8);
-	System.out.println(a.toString());
 	a.horWords();
 	a.verWords();
-	System.out.println(a.addScore("DOG"));
         System.out.println(a.getWords());
+	//a.clear();
+	a.addLetter("D", 11, 8);
+	a.addLetter("O", 11, 9);
+	a.horWordsAdded();
+	a.verWordsAdded();
+
+	System.out.println(a.getWordsAdded());
+	System.out.println(a.wordAdded());
+	// System.out.println(a.getWords());
+
+	
+	a.clearWords();
+
+	a.horWords();
+	a.verWords();
+        System.out.println(a.getWords());
+	
+	a.addLetter("O", 12, 8);
+	a.addLetter("T", 13, 8);
+
+
+	a.clearWordsAdded();
+	a.horWordsAdded();
+	a.verWordsAdded();
+
+	System.out.println(a.getWordsAdded());
+	System.out.println(a.wordAdded());
+	
+	System.out.println(a.toString());
+
+	
+
+
+	System.out.println(a.addScore1("DOG"));
 	System.out.println(a.checkAllWords());
+	
 	//	System.out.println(checkWord("sghheu"));
 
     }
